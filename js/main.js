13bfd3a7de50b9d8b8367fec8894817c74710e96
Saurabh undre <saurabh@fountaintechies.com>
2016-11-06 17:20:37 +0530

    second commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function main() {
+
+(function () {
+  // jQuery Parallax
+  function initParallax() {
+    $('#intro').parallax("100%", 0.1);
+    $('#services').parallax("100%", 0.1);
+    $('#aboutimg').parallax("100%", 0.1);	
+    $('#testimonials').parallax("100%", 0.1);
+    $('#myid').parallax("100%", 0.1);
+    $('#learn-app').parallax("100%", 0.1);
+    $('#future-web').parallax("100%", 0.1);
+    $('#about-me').parallax("100%", 0.1);
+    $('#freecharge-android').parallax("100%", 0.1);
+    
+
+  }
+  initParallax();
+
+
+}());
+
+
+}
+main();
